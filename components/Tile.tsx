27d7de67795aa0d3a808cb0f37c26f0a1601173f
Kevin <65783003+kewarrie@users.noreply.github.com>
2024-08-25 04:00:44 +0300

    Third draft layout

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -6,7 +6,7 @@
 
 import type { VoteProps } from '@/lib/utils';
 
-import { Avatar, Badge, Box, Card, Group, Space, Stack, Text } from '@mantine/core';
+import { Avatar, Badge, Center, Card, Group, Stack, Text } from '@mantine/core';
 
 interface TileProps {
   tessera: VoteProps;
@@ -17,32 +17,38 @@ export default function Tile({ tessera, baseUrl }: TileProps) {
   
   return (
     <Card radius="md" className="h-full" withBorder>
-      <Stack h={200} p="md" align="stretch" justify="space-around" gap="md">
-        <Card.Section>
+      <Stack h={200} align="stretch" justify="space-around" gap="sm">
+        <Card.Section p="sm" withBorder>
           <Group justify="space-between" align="flex-start">
             <Avatar
-              size={80}
+              size={60}
               src={`${baseUrl}/${tessera.id}/${tessera.avatar}`}
               alt={tessera.full_name}
-              name={tessera.full_name.split(/\s/).reduce((response,word)=> response+=word.slice(0,1),'')}
+              name={tessera.full_name}
+              color="initials"
             />
-            {tessera.vote === 'YES' && (<Badge variant="light" color="red" size="xl">Yes</Badge>)}
-            {tessera.vote === 'NO' && (<Badge variant="light" color="green" size="xl">No</Badge>)}
-            {tessera.vote === 'ABSENT' && (<Badge variant="light" color="blue" size="xl">ABSENT</Badge>)}
-            {tessera.vote === 'UNKOWN' && (<Badge variant="light" color="yellow" size="xl">UNKOWN</Badge>)}
-            {tessera.vote === 'DECEASED' && (<Badge variant="light" color="grey" size="xl">DECEASED</Badge>)}
+            <Stack align="flex-end">
+              {tessera.vote === 'YES' && (<Badge variant="light" color="red" size="xl">Yes</Badge>)}
+              {tessera.vote === 'NO' && (<Badge variant="light" color="green" size="xl">No</Badge>)}
+              {tessera.vote === 'ABSENT' && (<Badge variant="light" color="blue" size="xl">ABSENT</Badge>)}
+              {tessera.vote === 'UNKOWN' && (<Badge variant="light" color="yellow" size="xl">UNKOWN</Badge>)}
+              {tessera.vote === 'DECEASED' && (<Badge variant="light" color="grey" size="xl">DECEASED</Badge>)}
+
+              {tessera.location ? (<Text size="sm" c="dark.3" tt="capitalize">{tessera.location}</Text>) : (<Text size="sm" c="dark.4" tt="capitalize">N/A</Text>)}
+            </Stack>
           </Group>
         </Card.Section>
 
-        <Card.Section>
-          <Text size="sm">{tessera.full_name}</Text>
+        <Card.Section p="sm" withBorder>
+          <Center h={45}>
+            <Text size="xs" ta="center">{tessera.full_name}</Text>
+          </Center>
         </Card.Section>
 
-        <Card.Section>
-          {tessera.location && (<Text size="sm" c="dark.3" tt="capitalize">{tessera.location}</Text>)}
-          {tessera.represents === 'CONSTITUENCY' && (<Text size="sm" c="dimmed" tt="uppercase">Member of Parliament</Text>)}
-          {tessera.represents === 'COUNTY' && (<Text size="sm" c="dimmed" tt="uppercase">County Woman Representative</Text>)}
-          {tessera.represents === 'NOMINATED' && (<Text size="sm" c="dimmed" tt="uppercase">Nominated Representative</Text>)}
+        <Card.Section p="sm">
+          {tessera.represents === 'CONSTITUENCY' && (<Text size="sm" c="dimmed" tt="uppercase" ta="center">Member of Parliament</Text>)}
+          {tessera.represents === 'COUNTY' && (<Text size="sm" c="dimmed" tt="uppercase" ta="center">County Woman Representative</Text>)}
+          {tessera.represents === 'NOMINATED' && (<Text size="sm" c="dimmed" tt="uppercase" ta="center">Nominated Representative</Text>)}
         </Card.Section>
       </Stack>
     </Card>
