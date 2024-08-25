8b30eccdf070adedee1d961788fd9627821df1e1
Kevin <65783003+kewarrie@users.noreply.github.com>
2024-08-25 13:41:28 +0300

    Update to the Tile layout

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -18,8 +18,9 @@ export default function Tile({ tessera, baseUrl }: TileProps) {
   return (
     <Card radius="md" className="h-full" withBorder>
       <Stack h={200} align="stretch" justify="space-around" gap="sm">
-        <Card.Section p="sm" withBorder>
+        <Card.Section p="sm">
           <Group justify="space-between" align="flex-start">
+            {/* Elected Official Portrait */}
             <Avatar
               size={60}
               src={`${baseUrl}/${tessera.id}/${tessera.avatar}`}
@@ -28,24 +29,27 @@ export default function Tile({ tessera, baseUrl }: TileProps) {
               color="initials"
             />
             <Stack align="flex-end">
+              {/* Second Reading Vote */}
               {tessera.vote === 'YES' && (<Badge variant="light" color="red" size="xl">Yes</Badge>)}
               {tessera.vote === 'NO' && (<Badge variant="light" color="green" size="xl">No</Badge>)}
               {tessera.vote === 'ABSENT' && (<Badge variant="light" color="blue" size="xl">ABSENT</Badge>)}
               {tessera.vote === 'UNKOWN' && (<Badge variant="light" color="yellow" size="xl">UNKOWN</Badge>)}
               {tessera.vote === 'DECEASED' && (<Badge variant="light" color="grey" size="xl">DECEASED</Badge>)}
-
+              {/* Location */}
               {tessera.location ? (<Text size="sm" c="dark.3" tt="capitalize">{tessera.location}</Text>) : (<Text size="sm" c="dark.4" tt="capitalize">N/A</Text>)}
             </Stack>
           </Group>
         </Card.Section>
 
         <Card.Section p="sm" withBorder>
+          {/* Full Name */}
           <Center h={45}>
-            <Text size="xs" ta="center">{tessera.full_name}</Text>
+            <Text ta="center">{tessera.full_name}</Text>
           </Center>
         </Card.Section>
 
         <Card.Section p="sm">
+          {/* Representation */}
           {tessera.represents === 'CONSTITUENCY' && (<Text size="sm" c="dimmed" tt="uppercase" ta="center">Member of Parliament</Text>)}
           {tessera.represents === 'COUNTY' && (<Text size="sm" c="dimmed" tt="uppercase" ta="center">County Woman Representative</Text>)}
           {tessera.represents === 'NOMINATED' && (<Text size="sm" c="dimmed" tt="uppercase" ta="center">Nominated Representative</Text>)}
