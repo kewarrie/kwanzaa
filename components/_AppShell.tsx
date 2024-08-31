c5dcab032fa79d05a0c709367a191e0604a2cee8
Kevin <65783003+kewarrie@users.noreply.github.com>
2024-08-31 18:21:14 +0300

    Updates to Navbar

--- components/_AppShell.tsx
+++ components/_AppShell.tsx
@@ -12,15 +12,16 @@ import cx from 'clsx';
 
 import { usePathname } from 'next/navigation';
 
-import { ActionIcon, AppShell, Burger, Divider, Group, NavLink, Text, useMantineColorScheme, useComputedColorScheme } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks'
+import { ActionIcon, AppShell, Burger, Divider, Group, Space, Text, useMantineColorScheme, useComputedColorScheme } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
-import { IconSun, IconMoonStars } from '@tabler/icons-react';
+import { IconBook, IconMoonStars, IconSmartHome, IconSun } from '@tabler/icons-react';
 
 export default function _AppShell({ children }: { children: React.ReactNode; }) {
   // Change app theme
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true });
+  console.log(computedColorScheme);
 
   // Routing
   const pathname = usePathname();
@@ -30,16 +31,22 @@ export default function _AppShell({ children }: { children: React.ReactNode; }) 
   const [desktopOpened, { toggle: toggleDesktop }] =useDisclosure();    
 
   return (
-    <AppShell header={{ height: 60 }} navbar={{ width: 100, breakpoint: 'sm', collapsed: { mobile: !mobileOpened, desktop: !desktopOpened } }} padding="md">
+    <AppShell header={{ height: 60 }} navbar={{ width: 80, breakpoint: 'sm', collapsed: { mobile: !mobileOpened, desktop: !desktopOpened } }} padding="md">
       
       <AppShell.Header>
         <Group justify="space-between" h="100%" px="md">
           <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
           <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
-          {/* Logo - below "sm" then above "sm" */}
           <Text size="md">#RejectFinanceBill2024</Text>
-          {/* End of Logo */}
-          <ActionIcon variant="light" radius="xl" aria-label="Change theme" size="lg" onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}>
+          <ActionIcon
+            variant="light"
+            radius="xl"
+            size="lg"
+            aria-label="Change theme"
+            c={computedColorScheme === 'light' ? 'purple' : 'yellow'}
+            color={computedColorScheme === 'light' ? 'purple' : 'yellow'}
+            onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+          >
             <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
             <IconMoonStars className={cx(classes.icon, classes.dark)} stroke={1.5} />
           </ActionIcon>
@@ -47,9 +54,39 @@ export default function _AppShell({ children }: { children: React.ReactNode; }) 
       </AppShell.Header>
 
       <AppShell.Navbar p="md">
-        <NavLink href="/" label="home" ta="center" c="blue" color="blue" active={pathname == '/'} />
-        <Divider my="xl" />
-        <NavLink href="/about" label="about" ta="center" c="blue" color="blue" active={pathname == '/about'} />   
+        
+        <Text ta="center">
+          <ActionIcon
+            variant="light"
+            radius="xl"
+            size="xl"
+            aria-label="Home"
+            component="a"
+            href="/"
+            c="orange"
+            color="orange"
+          >
+            <IconSmartHome />
+          </ActionIcon>
+        </Text>
+        
+        <Space my="md" />
+        
+        <Text ta="center">
+        <ActionIcon
+            variant="light"
+            radius="xl"
+            size="xl"
+            aria-label="About"
+            component="a"
+            href="/about"
+            c="orange"
+            color="orange"
+          >
+            <IconBook />
+          </ActionIcon>
+        </Text>
+        
       </AppShell.Navbar>
 
       <AppShell.Main>
